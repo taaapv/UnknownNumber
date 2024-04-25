a1c16ec5afd256ffcbd0c251b38fcff5b47d506d
Tatiana <taaapv@gmail.com>
2024-04-25 16:18:56 +0400

    Храним загаданное число в виде строки Сообщение о выигрыше после цикла

--- UnknownNumber/main.swift
+++ UnknownNumber/main.swift
@@ -6,7 +6,7 @@
 //
 
 import Foundation
-let randomNumber = UInt8(arc4random_uniform(50))
+let randomNumber = String(arc4random_uniform(5))
 
 print("Компьютер случайным образом загадал число. Вам требуется его отгадать.")
 
@@ -14,15 +14,14 @@ var myNumber: String?
 repeat {
 	print("Введите ваш вариант числа")
 	myNumber = readLine()
-	guard let myNumber = myNumber, let myNumberInt8 = UInt8(myNumber) else {
+	guard let myNumber = myNumber else {
 		print("Некорректный ввод числа")
 		continue
 	}
-	if myNumberInt8 == randomNumber {
-		print("Вы угадали")
-	} else if myNumberInt8 < randomNumber {
+	if myNumber < randomNumber {
 		print("Загаданное число больше")
-	} else if myNumberInt8 > randomNumber {
+	} else if myNumber > randomNumber {
 		print("Загаданное число меньше")
 	}
-} while UInt8(myNumber!) != randomNumber
+} while myNumber != randomNumber
+print("Вы угадали")
